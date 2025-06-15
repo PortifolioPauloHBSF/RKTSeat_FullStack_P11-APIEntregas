5831d4374edd25c92feffd16016ba80b100e6379
PauloHBSF <paulohbsfilho@gmail.com>
2025-06-15 18:26:44 -0300

    :sparkles: validação incial dos dados de usuário recebidos

--- src/controllers/users-controllers.ts
+++ src/controllers/users-controllers.ts
@@ -1,7 +1,16 @@
 import { Request, Response } from "express";
+import { z } from "zod";
 
 class UsersController {
     create(request: Request, response: Response): any {
+        const bodySchema = z.object({
+            name: z.string().trim().min(2),
+            email: z.string().trim().email(),
+            password: z.string().trim().min(6),
+        });
+
+        const { name, email, password } = bodySchema.parse(request.body);
+        console.log(name, email, password)
         return response.json({ message: "Ok" });
     }
 }
