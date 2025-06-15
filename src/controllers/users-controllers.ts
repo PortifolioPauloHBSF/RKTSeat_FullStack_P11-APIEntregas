d5b1812e6ad4a40e843efcb496413350577f6cbc
PauloHBSF <paulohbsfilho@gmail.com>
2025-06-15 19:53:41 -0300

    :sparkles: retornando as informações de cadastro sem a senha criptografada

--- src/controllers/users-controllers.ts
+++ src/controllers/users-controllers.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 import { hash } from "bcrypt";
 import { z } from "zod";
+import { prisma } from "@/database/prisma";
+import { AppError } from "@/utils/AppError";
 
 class UsersController {
     async create(request: Request, response: Response): Promise<any> {
@@ -11,9 +13,27 @@ class UsersController {
         });
 
         const { name, email, password } = bodySchema.parse(request.body);
-        const hashedPassword = await hash(password, Number(process.env.PASSWORD_HASH));
 
-        return response.json({ message: "Ok", hashedPassword });
+        // Validando se o email já está cadastrado.
+        const userWithSameEmail = await prisma.user.findFirst({ where: { email } });
+        if (userWithSameEmail) {
+            throw new AppError("Email already in use.");
+        }
+
+        // Criptografando a senha do usuário e salvando no banco.
+        const hashedPassword = await hash(password, Number(process.env.PASSWORD_HASH));
+        const user = await prisma.user.create({
+            data: {
+                name,
+                email,
+                password: hashedPassword,
+            },
+        });
+
+        // Retornando apenas o essencial
+        const { password: _, ...userWithoutPassword } = user
+
+        return response.json(userWithoutPassword);
     }
 }
 
--- src/database/prisma.ts
+++ src/database/prisma.ts
@@ -1,6 +1,6 @@
 // Configuraçoes de conn com o prisma.
 
-import { PrismaClient } from "@prisma/client/extension";
+import { PrismaClient } from "@/generated/prisma";
 
 export const prisma = new PrismaClient({
     log: process.env.NODE_ENV === "production" ? [] : ["query"],
