e21de12223447f2fd5a8d68324f95271bcffb878
PauloHBSF <paulohbsfilho@gmail.com>
2025-06-15 18:59:04 -0300

    :wrench: configurando o prisma client

---
+++ src/database/prisma.ts
@@ -0,0 +1,7 @@
+// Configuraçoes de conn com o prisma.
+
+import { PrismaClient } from "@prisma/client/extension";
+
+export const prisma = new PrismaClient({
+    log: process.env.NODE_ENV === "production" ? [] : ["query"],
+});
