5f39b6653bcdd2e189b1fcd69b541a3c97530e43
PauloHBSF <paulohbsfilho@gmail.com>
2025-06-19 23:22:50 -0300

    :sparkles: implementado middleware de verifica a autorização de um usuário

--- src/routes/deliveries-routes.ts
+++ src/routes/deliveries-routes.ts
@@ -1,13 +1,15 @@
 import { Router } from "express";
 import { DeliveriesController } from "@/controllers/deliveries-controller";
 import { ensureAuthenticated } from "@/middlewares/ensure-authenticated";
+import { verifyUserAuthorization } from "@/middlewares/verify-user-authorization";
 
 const deliveryRoutes = Router();
 const deliveriesController = new DeliveriesController();
 
 // Passa pelo nosso fluxo de auth
 deliveryRoutes.use(ensureAuthenticated);
+deliveryRoutes.use(verifyUserAuthorization(["sale"]));
 
-deliveryRoutes.post('/', deliveriesController.create)
+deliveryRoutes.post("/", deliveriesController.create);
 
-export { deliveryRoutes }
+export { deliveryRoutes };
