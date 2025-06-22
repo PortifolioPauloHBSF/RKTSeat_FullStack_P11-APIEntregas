90cfd75dddfe5456acfe32216a8b3962583161e5
PauloHBSF <paulohbsfilho@gmail.com>
2025-06-22 12:29:49 -0300

    :white_check_mark: adicionado teste de cadastro de usuário

--- src/tests/users-controller.test.ts
+++ src/tests/users-controller.test.ts
@@ -1,3 +1,25 @@
+import request from "supertest";
+import { app } from "@/app";
+import { prisma } from "@/database/prisma";
+
 describe("UsersController", () => {
-    it("make a simple console log", () => {console.log("O Jest está funcionando!")})
-})
+    let user_id: string;
+
+    it("should create a new user sucessfully", async () => {
+        const response = await request(app).post("/users").send({
+            name: "Test User",
+            email: "testuser@example.com",
+            password: "password123",
+        });
+        expect(response.status).toBe(201);
+        expect(response.body).toHaveProperty("id");
+        expect(response.body.name).toBe("Test User");
+
+        // Salvando o ID gerado ao gravar usuário.
+        user_id = response.body.id;
+    });
+
+    afterAll(async () => {
+        await prisma.user.delete({ where: { id: user_id } });
+    });
+});
