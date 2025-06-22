42c85dd25f2cd66726d5f84056bed0402141816b
PauloHBSF <paulohbsfilho@gmail.com>
2025-06-21 23:01:28 -0300

    :sparkles: implementando logs de um delivery movimentando

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { usersRoutes } from "@/routes/users-routes";
 import { sessionRouters } from "@/routes/session-routes";
-import { deliveryRoutes } from "./deliveries-routes";
+import { deliveryRoutes } from "@/routes/deliveries-routes";
+import { deliveryLogsRoutes } from "@/routes/delivery-logs-routes";
 
 const routes = Router();
 routes.use("/users", usersRoutes);
 routes.use("/sessions", sessionRouters);
 routes.use("/deliveries", deliveryRoutes);
+routes.use("/delivery-logs", deliveryLogsRoutes);
 
 export { routes };
