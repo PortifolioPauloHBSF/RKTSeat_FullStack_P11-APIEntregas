4866bd8414c49c8b437bcc5e444ea15fe5f83602
PauloHBSF <paulohbsfilho@gmail.com>
2025-06-21 22:26:34 -0300

    :sprakles: implementado alterar status de uma entrega

--- src/routes/deliveries-routes.ts
+++ src/routes/deliveries-routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { DeliveriesController } from "@/controllers/deliveries-controller";
+import { DeliveriesStatusController } from "@/controllers/deliveries-status-controller";
 import { ensureAuthenticated } from "@/middlewares/ensure-authenticated";
 import { verifyUserAuthorization } from "@/middlewares/verify-user-authorization";
 
 const deliveryRoutes = Router();
 const deliveriesController = new DeliveriesController();
+const deliveriesStatusController = new DeliveriesStatusController();
 
 // Passa pelo nosso fluxo de auth
 deliveryRoutes.use(ensureAuthenticated);
@@ -12,5 +14,7 @@ deliveryRoutes.use(verifyUserAuthorization(["sale"]));
 
 deliveryRoutes.get("/", deliveriesController.index);
 deliveryRoutes.post("/", deliveriesController.create);
+
+deliveryRoutes.patch("/:id/status", deliveriesStatusController.update);
 
 export { deliveryRoutes };
