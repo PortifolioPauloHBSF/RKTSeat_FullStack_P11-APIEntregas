1b8bd3fe5265b3c67338d08690fc750fd6ffc74a
PauloHBSF <paulohbsfilho@gmail.com>
2025-06-22 13:25:08 -0300

    :white_check_mark: adicionado teste que verifica o retorno do token do usuario

---
+++ src/tests/sessions-controller.test.ts
@@ -0,0 +1,28 @@
+import request from "supertest";
+import { app } from "@/app";
+import { prisma } from "@/database/prisma";
+
+describe("SessionsController", () => {
+    let user_id: string;
+    it("should authenticate and get access token", async () => {
+        const userResponse = await request(app).post("/users").send({
+            name: "Auth Test User",
+            email: "authtestuser@example.com",
+            password: "password123",
+        });
+
+        user_id = userResponse.body.id;
+
+        const sessionResponse = await request(app).post("/sessions").send({
+            email: "authtestuser@example.com",
+            password: "password123",
+        });
+
+        expect(sessionResponse.status).toBe(200);
+        expect(sessionResponse.body.token).toEqual(expect.any(String));
+    });
+
+    afterAll(async () => {
+        await prisma.user.delete({ where: { id: user_id } });
+    });
+});
